7819261fa3a71cb8f8f9817225646d3a82ca2cc2
Neer Khandor <neerkhandor456@gmail.com>
2023-12-29 11:39:17 +0530

    Changes Updated

--- currency_converter/src/hooks/useCurrencyInfo.js
+++ currency_converter/src/hooks/useCurrencyInfo.js
@@ -3,11 +3,17 @@ import { useEffect,useState } from "react";
 function useCurrencyInfo(currency){
     const[data,setData]=useState({})
     useEffect(()=>{
-        fetch(`https://cdn.jsdeliver.net/gh/fawazahmed0/currency-api@/latest/currencies/${currency}.json`)
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
         .then((res)=>res.json())
         .then((res)=>setData(res.currency))
         console.log(data);
         console.log(label)
-    })
+    },[currency])
+    return data;
     
 }
+export default useCurrencyInfo;
+
+// data ko agar regular variable mein hold karenge to problem hoga wo ui mein kabhi update nahi hoga
+// so ye hame useState se value return karvata hai and agar kuch value na aaye to empty obj return hojaye
+// so currency ke andar kabhi bhi change ho to value change hoti hai ye ek dependency hai
